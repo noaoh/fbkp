04d890be4323b1a53059ba89de4181138a03d6f3
Noah Holt <noahryanholt@gmail.com>
2018-07-21 16:35:38 -0500

    Return errors instead of logging

--- fbkp.go
+++ fbkp.go
@@ -1,7 +1,6 @@
 package main 
 
 import (
-        "log"
         "os"
         "io/ioutil"
 )
@@ -9,24 +8,24 @@ import (
 func BackupFile(name string, suffix string) error {
         og_file, err := os.Open(name)
         if err != nil {
-                log.Fatal(err)
+                return err
         }
         defer og_file.Close()
 
         buf, err := ioutil.ReadAll(og_file)
         if err != nil {
-                log.Fatal(err)
+                return err
         }
 
         bkp_name := name + "." + suffix
         _, err = os.Create(bkp_name)
         if err != nil {
-                log.Fatal(err)
+                return err
         }
 
         err = os.Chmod(bkp_name, 0644)
         if err != nil {
-                log.Fatal(err)
+                return err
         }
 
         err = ioutil.WriteFile(bkp_name, buf, 0755)
@@ -41,18 +40,18 @@ func RestoreFile(name string, suffix string) error {
         bkp_name := name + "." + suffix
         bkp_file, err := os.Open(bkp_name)
         if err != nil {
-                log.Fatal(err)
+                return err
         }
         defer bkp_file.Close()
 
         buf, err := ioutil.ReadAll(bkp_file)
         if err != nil {
-                log.Fatal(err)
+                return err
         }
 
         err = ioutil.WriteFile(name, buf, 0755)
         if err != nil {
-                log.Fatal(err)
+                return err
         }
         return err
 }
