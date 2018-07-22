3691c1ae26518f57babc2be47f2649662e8d16c7
Noah Holt <noahryanholt@gmail.com>
2018-07-21 19:23:56 -0500

    Use shell glob instead of golang glob

--- fbkp.go
+++ fbkp.go
@@ -1,57 +1,82 @@
-package main 
+package main
 
 import (
-        "os"
-        "io/ioutil"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func BackupFile(name string, suffix string) error {
-        og_file, err := os.Open(name)
-        if err != nil {
-                return err
-        }
-        defer og_file.Close()
+	og_file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer og_file.Close()
 
-        buf, err := ioutil.ReadAll(og_file)
-        if err != nil {
-                return err
-        }
+	buf, err := ioutil.ReadAll(og_file)
+	if err != nil {
+		return err
+	}
 
-        bkp_name := name + "." + suffix
-        _, err = os.Create(bkp_name)
-        if err != nil {
-                return err
-        }
+	bkp_name := name + "." + suffix
+	_, err = os.Create(bkp_name)
+	if err != nil {
+		return err
+	}
 
-        err = os.Chmod(bkp_name, 0644)
-        if err != nil {
-                return err
-        }
+	err = os.Chmod(bkp_name, 0644)
+	if err != nil {
+		return err
+	}
 
-        err = ioutil.WriteFile(bkp_name, buf, 0755)
-        if err != nil {
-                return err
-        }
+	err = ioutil.WriteFile(bkp_name, buf, 0755)
+	if err != nil {
+		return err
+	}
 
-        return err 
+	return err
 }
 
 func RestoreFile(name string, suffix string) error {
-        bkp_name := name + "." + suffix
-        bkp_file, err := os.Open(bkp_name)
-        if err != nil {
-                return err
-        }
-        defer bkp_file.Close()
+	bkp_name := name + "." + suffix
+	bkp_file, err := os.Open(bkp_name)
+	if err != nil {
+		return err
+	}
+	defer bkp_file.Close()
 
-        buf, err := ioutil.ReadAll(bkp_file)
-        if err != nil {
-                return err
-        }
+	buf, err := ioutil.ReadAll(bkp_file)
+	if err != nil {
+		return err
+	}
 
-        err = ioutil.WriteFile(name, buf, 0755)
-        if err != nil {
-                return err
-        }
-        return err
+	err = ioutil.WriteFile(name, buf, 0755)
+	if err != nil {
+		return err
+	}
+	return err
 }
+
+func RestoreDir(dir string, suffix string) error {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	real_suffix := "." + suffix
+	for _, file := range files {
+		fname := file.Name()
+
+		if filepath.Ext(fname) == real_suffix {
+			restore_file := strings.TrimSuffix(fname, real_suffix)
+
+			err := RestoreFile(restore_file, suffix)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return err
+}
+
--- main.go
+++ main.go
@@ -1,58 +1,46 @@
-package main 
+package main
+
 import (
-        "fmt"
-        "flag"
-        "os"
-        "path/filepath"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-        file := flag.String("f", "", "The file(s) to backup")
-        ext := flag.String("e", "bak", "The name of the backup extension")
-        restore := flag.Bool("r", false, "Restores the file from the backup if true, otherwise it backs the file up")
-        verbose := flag.Bool("v", false, "Verbosely prints output")
+	ext := flag.String("e", "bak", "The name of the backup extension")
+	restore := flag.Bool("r", false, "Restores the file from the backup if true, otherwise it backs the file up")
+	verbose := flag.Bool("v", false, "Verbosely prints output")
 
-        flag.Parse()
+	flag.Parse()
 
-        if flag.NFlag() == 0 {
-                fmt.Println("Usage of fbkp:")
-                flag.PrintDefaults()
-                os.Exit(0)
-        }
+	if flag.NFlag() == 0 && flag.NArg() == 0 {
+		fmt.Println("Usage of fbkp:")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
 
-        if *file == "" {
-                fmt.Println("No file(s) passed as an argument")
-                os.Exit(1)
-        }
+        for _, file := range flag.Args() {
+		bak_filename := file + "." + *ext
+		if *restore {
+			err := RestoreFile(file, *ext)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 
-        files, err := filepath.Glob(*file)
-        if err != nil {
-                fmt.Println(err)
-        }
+			if *verbose {
+				fmt.Printf("%q -> %q\n", bak_filename, file)
+			}
+		} else {
+			err := BackupFile(file, *ext)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 
-
-        for _, file := range files {
-                bak_filename := file + "." + *ext
-                if *restore {
-                        err := RestoreFile(file, *ext)
-                        if err != nil {
-                                fmt.Println(err)
-                                os.Exit(1)
-                        }
-                        
-                        if *verbose {
-                                fmt.Printf("%q -> %q\n", bak_filename, file)
-                        }
-                } else {
-                        err := BackupFile(file, *ext)
-                        if err != nil {
-                                fmt.Println(err)
-                                os.Exit(1)
-                        }
-
-                        if *verbose {
-                                fmt.Printf("%q -> %q\n", file, bak_filename)
-                        }
-                }
-        }
+			if *verbose {
+				fmt.Printf("%q -> %q\n", file, bak_filename)
+			}
+		}
+	}
 }
